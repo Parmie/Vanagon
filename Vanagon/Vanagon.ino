d0bc088295fcd1ae6c97fb9b48251af2d86c4bb3
Tom Parmentier <tom-parmentier@db-engineering.be>
2017-01-31 23:01:46 +0100

    Created a demo display with sensor values.

--- Vanagon/Vanagon.ino
+++ Vanagon/Vanagon.ino
@@ -14,7 +14,14 @@ void setup()
 {
 	display.begin(55); // Contrast 55 seems to be the best for Nokia 5110
 	display.clearDisplay();
-	writeLine(0, 0, "Ready");
+	writeString(0, 0, "RPM:");
+	writeString(0, 7, "2800");
+	writeString(1, 0, "Temp:");
+	writeString(1, 7, "89 C");
+	writeString(2, 0, "Speed:");
+	writeString(2, 7, "72 kmh");
+	writeString(3, 0, "Fuel:");
+	writeString(3, 7, "34 %");
 	display.display();
 }
 
@@ -22,15 +29,15 @@ void loop()
 {
 }
 
-void drawCharacter(uint8_t line, uint8_t pos, char c)
+void writeCharacter(uint8_t line, uint8_t pos, char c)
 {
 	display.drawChar(pos * 6, line * 10, c, 1, 0, 1);
 }
 
-void writeLine(uint8_t line, uint8_t pos, String text)
+void writeString(uint8_t line, uint8_t pos, String text)
 {
 	for (uint8_t i = 0; i < text.length(); i++)
 	{
-		drawCharacter(line, pos + i, text[i]);
+		writeCharacter(line, pos + i, text[i]);
 	}
 }
