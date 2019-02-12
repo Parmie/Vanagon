c4d4c3a5e4b8ce763382741d6d49b4f6087ac35c
Tom Parmentier <tom.parmentier@spirics.be>
2019-02-12 22:22:30 +0100

    Addes cycle time to graphs. Scaling, steps and cycle time on graph.

--- src/Vanagon/GraphView.cpp
+++ src/Vanagon/GraphView.cpp
@@ -3,24 +3,38 @@
 
 #include <Arduino.h>
 #include "Display.cpp"
+#include "PerformanceMonitor.cpp"
+
+#define GRAPH_LENGTH 82
+#define GRAPH_HIGHT 38
 
 class GraphView : public View {
   private:
     String _title;
-    byte _points[84];
+    PerformanceMonitor *_performanceMonitor;
+    float _min;
+    float _max;
+    float _step1;
+    float _step2;
+    byte _points[GRAPH_LENGTH];
     byte _firstPoint = 0;
 
   public:
-    GraphView(String title)
+    GraphView(String title, PerformanceMonitor *performanceMonitor, float min = 0, float max = GRAPH_HIGHT, float step1 = 0, float step2 = 0)
     {
       _title = title;
+      _performanceMonitor = performanceMonitor;
+      _min = min;
+      _max = max;
+      _step1 = step1;
+      _step2 = step2;
     };
 
-    void addPoint(byte value)
+    void addPoint(float value)
     {
-      _points[_firstPoint] = value;
+      _points[_firstPoint] = (value - _min) * GRAPH_HIGHT / _max;
       _firstPoint++;
-      if (_firstPoint == 84)
+      if (_firstPoint == GRAPH_LENGTH)
       {
         _firstPoint = 0;
       }
@@ -29,19 +43,47 @@ class GraphView : public View {
     void draw(Display *display) override
     {
       display->writeString(0, 0, _title);
-      for (byte x=0; x < 84; x++)
+      String cycleTimeText = String(_performanceMonitor->getCycleTime());
+      display->writeString(0, 12-cycleTimeText.length(), cycleTimeText);
+      display->writeString(0, 12, "ms");
+      /*
+      for (byte x=0; x < GRAPH_LENGTH; x++)
       {
-        display->drawPixel(x, 9);
+        display->drawPixel(x, 8);
+      }
+      */
+
+      if (_step1 > 0)
+      {
+        for (float i = _min; i <= _max; i = i + _step1)
+        {
+          byte value = (i - _min) * GRAPH_HIGHT / _max;
+          display->drawPixel(0, 47 - value);
+        }
       }
 
-      for (byte i=0; i < 84; i++)
+      if (_step2 > 0)
+      {
+        for (float i = _min; i <= _max; i = i + _step2)
+        {
+          byte value = (i - _min) * GRAPH_HIGHT / _max;
+          display->drawPixel(0, 47 - value);
+          display->drawPixel(1, 47 - value);
+        }
+      }
+
+      for (byte i=0; i < GRAPH_LENGTH; i++)
       {
         byte index = _firstPoint+i;
-        if (index > 83)
+        if (index >= GRAPH_HIGHT)
         {
-          index = index - 84;
+          index = index - GRAPH_HIGHT;
         }
-        display->drawPixel(i, 47 - _points[i]);
+        byte value = _points[i];
+        if (value >= 0 && value <= GRAPH_HIGHT)
+        {
+          display->drawPixel(2+i, 47 - _points[i]);
+        }
       }
     };
 };
